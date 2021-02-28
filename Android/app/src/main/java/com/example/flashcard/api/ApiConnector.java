363e2d33f40e8203001f373f574f6799542cc6f3
KthProg <kthprog@gmail.com>
2021-02-28 15:28:03 -0500

    waow

--- Android/app/src/main/java/com/example/flashcard/api/ApiConnector.java
+++ Android/app/src/main/java/com/example/flashcard/api/ApiConnector.java
@@ -1,6 +1,7 @@
 package com.example.flashcard.api;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -12,7 +13,9 @@ public class ApiConnector {
     public static void getSets(Context ctx, Response.Listener<String> listener){
         RequestQueue queue = Volley.newRequestQueue(ctx);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, "http://10.0.2.2:5000/sets", listener, null);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "http://10.0.2.2:5000/sets", listener, error -> {
+            Log.e("ApiConnector", error.getMessage());
+        });
 
         queue.add(stringRequest);
     }
